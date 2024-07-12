adede01683755fbfb19932a907724585a2d49135
Shrividya887 <151447062+Shrividya887@users.noreply.github.com>
2024-07-12 17:58:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import axios from 'axios';
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+export const getPlacesData = async (sw,ne) => {
+    try {
+            const { data: { data }}= await axios.get(URL, {
+                params: {
+                  bl_latitude: sw.lat,
+                  tr_latitude: ne.lat,
+                  bl_longitude: sw.lng,
+                  tr_longitude: ne.lng,
+                },
+                headers: {
+                  'X-RapidAPI-Key': '589b518a04mshdfccfdada8612dcp1c56b7jsn40c3e3a598ef',
+                  'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+                }
+              });
+              
+            return data;
+          } catch (error) {
+              console.log(error)
+          }
+}
